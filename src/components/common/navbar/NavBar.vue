ed086ac37365df61d67f4447ac587a121887bc0f
Mr-BQ <979243959@qq.com>
2020-11-13 16:57:13 +0800

    commit

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="navbar">
+        <div class="left"><slot name="left"></slot></div>
+        <div class="center"><slot name="center"></slot></div>
+        <div class="right"><slot name="right"></slot></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+    .nav-bar{
+        display: flex;
+        height: 44px;
+    }
+    .left,.right{
+        width:60px;
+        background-color: dodgerblue;
+    }
+    .center{
+        flex: 1;
+        background-color: lightskyblue;
+    }
+
+</style>
